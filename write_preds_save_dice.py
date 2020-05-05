2ed3e4c6eef53805d4bf84a94e346268a522c24e
jjia <972931271@qq.com>
2020-05-05 16:46:54 +0200

    fix bug

--- write_preds_save_dice.py
+++ write_preds_save_dice.py
@@ -9,6 +9,8 @@ from write_dice import write_dices_to_csv
 import segmentor as v_seg
 import os
 import re
+from train_ori_fit_rec_epoch import Mypath
+
 # from train_ori_fit_rec_epoch import Mypath
 #1583105196.4919333_0.00010a_o_0.5ds2dr1bn1fs16ptsz144ptzsz80
 #1583105073.503702_0.00010a_o_0.5ds2dr1bn1fs16ptsz144ptzsz80
@@ -57,49 +59,42 @@ str_names = [
              '1588289073.84265_0.00010a_o_0ds2dr1bn1fs4ptsz144ptzsz96',
              '1588289144.719585_0.00010a_o_0ds2dr1bn1fs16ptsz144ptzsz96']
 print(str_names)
+
+mypath = Mypath(task)
+
 for str_name in str_names:
     # str_name = '1585000573.7211952_0.00011a_o_0ds2dr1bn1fs16ptsz144ptzsz64'
     model_name = os.path.dirname (os.path.realpath (__file__)) +'/models/' + task + '/' + str_name + 'MODEL.hdf5'
     # ptch_z_sz = int(str_name.split('ptzsz')[-1])
     # ptch_sz = int(str_name.split('ptsz')[-1].split('ptzsz')[0])
 
-    ptch_z_sz = int(re.findall(r'\d+', str_name.split('ptzsz')[-1]))
-    ptch_sz = int(re.findall(r'\d+', str_name.split('ptsz')[-1]))
+    ptch_z_sz = int(re.findall(r'\d+', str_name.split('ptzsz')[-1])[0])
+    ptch_sz = int(re.findall(r'\d+', str_name.split('ptsz')[-1])[0])
+    tr_sp = float(re.findall(r'\d+', str_name.split('trsp')[-1])[0])
+    tr_z_sp = float(re.findall(r'\d+', str_name.split('trzsp')[-1])[0])
 
-    print('patch_sz', ptch_sz1, 'patch_z_size', ptch_z_sz1)
-
+    print('patch_sz', ptch_sz, 'patch_z_size', ptch_z_sz)
 
     for phase in ['valid']:
         if task=='lobe':
             labels = [0, 4, 5, 6, 7, 8]
-            data_dir = os.path.dirname (os.path.realpath (__file__)) + '/data/lobe/' + phase + '/ori_ct/GLUCOLD'
-            gdth_dir = os.path.dirname (os.path.realpath (__file__)) + '/data/lobe/'  + phase + '/gdth_ct/GLUCOLD'
-            preds_dir = os.path.dirname (os.path.realpath (__file__)) + '/data/lobe/preds/' + phase + '/GLUCOLD/' + \
-                        model_name.split ('/')[-1][:8]
-            # print (preds_dir)
         elif task=='vessel':
             labels = [0, 1]
-            data_dir = os.path.dirname (os.path.realpath (__file__)) + '/data/vessel/' + phase + '/ori_ct'
-            gdth_dir = os.path.dirname (os.path.realpath (__file__)) + '/data/vessel/'  + phase + '/gdth_ct'
-            preds_dir = os.path.dirname (os.path.realpath (__file__)) + '/results/vessel/'+ \
-                        model_name.split ('/')[-1][:8]
-            # print(preds_dir)
-            # preds_dir = '/data/jjia/e2e_new/results/vessel/test_dice'
 
         segment = v_seg.v_segmentor(batch_size=1,
                                     model=model_name,
                                     ptch_sz=ptch_sz, ptch_z_sz=ptch_z_sz,
                                     trgt_sz=None, trgt_z_sz=None,
-                                    trgt_space_list=[0.3, 0.5, 0.5],  # 2.5, 1.4, 1.4 [2.5, 1.4, 1.4],[0.5, 0.6, 0.6]
+                                    trgt_space_list=[tr_z_sp, tr_sp, tr_sp],  # 2.5, 1.4, 1.4 [2.5, 1.4, 1.4],[0.5, 0.6, 0.6]
                                     task=task)
 
         write_preds_to_disk(segment=segment,
-                            data_dir=data_dir,
-                            preds_dir=preds_dir,
+                            data_dir=mypath.ori_ct_path(phase),
+                            preds_dir=mypath.pred_path(phase),
                             number=3, stride=0.5)
 
 
         write_dices_to_csv (labels=labels,
-                            gdth_path=gdth_dir,
-                            pred_path=preds_dir,
-                            csv_file=preds_dir+'/dices.csv')
+                            gdth_path=mypath.gdth_path(phase),
+                            pred_path=mypath.pred_path(phase),
+                            csv_file=mypath.dices_location(phase))
