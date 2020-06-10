9ed4217e87653d7aea00be2c1cdd03895b9ff266
jjia <972931271@qq.com>
2020-06-11 00:32:17 +0200

    add docs

--- mypath.py
+++ mypath.py
@@ -1,4 +1,3 @@
-
 from set_args import args
 import os
 import time
@@ -7,6 +6,12 @@ from functools import wraps
 
 
 def mkdir_dcrt(fun): # decorator to create directory if not exist
+    """
+    A decorator to make directory output from function if not exist.
+
+    :param fun: a function which outputs a directory
+    :return: decorated function
+    """
     @wraps(fun)
     def decorated(*args, **kwargs):
         output = fun(*args, **kwargs)
@@ -25,12 +30,19 @@ def mkdir_dcrt(fun): # decorator to create directory if not exist
     return decorated
 
 
-class Mypath:
-    '''
-    Here, I use 'location' to indicatate full file path and name, 'path' to respresent the directory,
+class Mypath(object):
+    """
+    Here, I use 'fpath' to indicatate full file path and name, 'path' to respresent the directory,
     'file_name' to respresent the file name in the parent directory.
-    '''
+    """
     def __init__(self, task, current_time=None):
+
+        """
+        initial valuables.
+
+        :param task: task name, e.g. 'lobe'
+        :param current_time: a string represent the current time. It is used to name models and log files. If None, the time will be generated automatically.
+        """
 
         self.task = task
         self.dir_path = os.path.dirname (os.path.realpath (__file__)) # abosolute path of the current script
@@ -66,6 +78,11 @@ class Mypath:
         self.str_name = self.current_time + self.setting
 
     def sub_dir(self):
+        """
+        Sub directory of tasks. It is used to choose different datasets (like 'GLUCOLD', 'SSc').
+
+        :return: sub directory name
+        """
 
         if self.task=='lobe':
             sub_dir = 'GLUCOLD'
@@ -78,75 +95,158 @@ class Mypath:
 
     @mkdir_dcrt
     def train_dir(self):
+        """
+        training directory.
+
+        :return: training dataset directory for a specific task
+        """
         train_dir = self.data_path + '/' + self.task + '/train'
         return train_dir
 
     @mkdir_dcrt
     def valid_dir(self):
+        """
+        validation directory.
+
+        :return: validation dataset directory for a specific task
+        """
         valid_dir = self.data_path + '/' + self.task + '/valid'
         return valid_dir
 
     @mkdir_dcrt
     def task_log_dir(self):
+        """
+        log directory.
+        :return: directory to save logs
+        """
         task_log_dir = self.log_path + '/' + self.task
         return task_log_dir
 
     @mkdir_dcrt
     def task_model_dir(self):
+        """
+        model directory.
+        :return: directory to save models
+        """
         task_model_dir = self.model_path + '/' + self.task
         return task_model_dir
 
     @mkdir_dcrt
     def log_fpath(self):
+        """
+        log full path.
+
+        :return: log full path with suffix .log
+        """
         task_log_dir = self.task_log_dir()
         return task_log_dir + '/' + self.str_name + '.log'
 
     @mkdir_dcrt
     def tr_va_log_fpath(self):
+        """
+        log full path to save training and validation measuremets during training.
+
+        :return: log full path with suffix .log
+        """
         task_log_dir = self.task_log_dir()
         return task_log_dir + '/' + self.str_name + 'tr_va.log'
 
     @mkdir_dcrt
     def train_log_fpath(self):
+        """
+        log full path to save training  measuremets during training.
+
+        :return: log full path with suffix .log
+        """
         task_log_dir = self.task_log_dir()
         return task_log_dir + '/' + self.str_name + 'train.log'
 
     @mkdir_dcrt
     def model_figure_path(self):
+        """
+        Directory where to save figures of model architecture.
+
+        :return: model figure directory
+        """
         model_figure_path = self.dir_path + '/figures'
         return model_figure_path
 
     @mkdir_dcrt
     def json_fpath(self):
+        """
+        full path of model json.
+
+        :return: model json full path
+        """
         task_model_path = self.task_model_dir()
         return task_model_path + '/' + self.str_name + 'MODEL.json'
 
     @mkdir_dcrt
     def best_va_loss_location(self):
+        """
+        full path to save best model according to validation loss.
+        :return: full path
+        """
+        task_model_path = self.task_model_dir()
+        return task_model_path + '/' + self.str_name + '_tr_best.hdf5'
+
+
+
+    @mkdir_dcrt
+    def model_fpath_best_train(self):
+        """
+        full path to save best model according to training loss.
+        :return: full path
+        """
         task_model_path = self.task_model_dir()
         return task_model_path + '/' + self.str_name + 'MODEL.hdf5'
 
     @mkdir_dcrt
-    def best_tr_loss_location(self):
+    def model_fpath_best_valid(self):
+        """
+        full path to save best model according to training loss.
+        :return: full path
+        """
         task_model_path = self.task_model_dir()
-        return task_model_path + '/' + self.str_name + '_tr_best.hdf5'
+        return task_model_path + '/' + self.str_name + 'MODEL_valid.hdf5'
+
 
     @mkdir_dcrt
     def ori_ct_path(self, phase='train'):
+        """
+        absolute directory of the original ct for training dataset
+        :param phase: 'train' or 'valid'
+        :return: directory name
+        """
         data_path = self.data_path + '/' + self.task + '/' + phase + '/ori_ct/' + self.sub_dir()
         return data_path
 
     @mkdir_dcrt
     def gdth_path(self, phase='train'):
+        """
+        absolute directory of the ground truth of ct for training dataset
+        :param phase: 'train' or 'valid'
+        :return: directory name
+        """
         gdth_path = self.data_path + '/' + self.task + '/' + phase + '/gdth_ct/' + self.sub_dir()
         return gdth_path
 
     @mkdir_dcrt
     def pred_path(self, phase='train'):
+        """
+        absolute directory of the prediction results of ct for training dataset
+        :param phase: 'train' or 'valid'
+        :return: directory name
+        """
         pred_path = self.results_path + '/' + self.task + '/' + phase + '/pred/' + self.sub_dir() + '/' + self.current_time
         return pred_path
 
     @mkdir_dcrt
     def dices_fpath(self, phase='train'):
+        """
+        full path of the saved dice
+        :param phase: 'train' or 'valid'
+        :return: file name to save dice
+        """
         pred_path = self.pred_path(phase)
         return self.results_path + '/' + self.task + '/' + phase + '/pred/' + self.sub_dir() + '/' + self.current_time + '/dices.csv'
