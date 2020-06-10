c23493abbf1dd85c518e0ac8fb1f9173e0e4d499
jjia <972931271@qq.com>
2020-06-11 00:31:11 +0200

    keep big parts to save time

--- find_connect_parts.py
+++ find_connect_parts.py
@@ -12,14 +12,30 @@ def largest_connected_parts(bw_img, nb_parts_saved=1):
 
     for i in range(1, num + 1):  # 0 respresent background
         connect_part = (labeled_img == i).astype(int)
-        connect_part_list.append(connect_part)
+
+
         pixel_count = np.sum(connect_part)
-        pixel_count_list.append(pixel_count)
-        pixel_label_list.append(i)
-    pixel_count_list, connect_part_list, pixel_label_list = zip(*sorted(zip(pixel_count_list, connect_part_list, pixel_label_list), reverse=True))
+        print('step', i, ' pixel number', pixel_count)
+        if pixel_count>30000:
+            print('-------*****-------')
+            pixel_count_list.append(pixel_count)
+
+            connect_part_list.append(connect_part)
+
+            pixel_label_list.append(i)
+
+
+
+    pixel_count_list_index = list(range(len(pixel_count_list)))
+    pixel_count_list_sorted, pixel_label_list_sorted = zip(*sorted(zip(pixel_count_list, pixel_label_list), reverse=True))
+
+    pixel_count_list_sorted, pixel_count_list_index_sorted = zip(
+        *sorted(zip(pixel_count_list, pixel_count_list_index), reverse=True))
+    # pixel_count_list_sorted, connect_part_list_sorted = zip(*sorted(zip(pixel_count_list, connect_part_list), reverse=True))
     out = np.zeros(bw_img.shape)
-    for i in range(nb_parts_saved):
-        out += connect_part_list[i]
+    for i, idx in zip(range(nb_parts_saved), pixel_count_list_index_sorted):
+        print('i, idx', i, idx)
+        out += connect_part_list[idx]
 
     return out
 
